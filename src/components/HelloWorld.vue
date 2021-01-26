d1fbdfde5b9c22e0fdaa0f8f9b6acfcb4bd5eada
luoyanfen <1529809238@qq.com>
2021-01-26 15:12:05 +0800

    修改2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,7 @@
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
-    <h3>Jenkins 测试</h3>
+    <h3>Jenkins 测试1</h3>
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
